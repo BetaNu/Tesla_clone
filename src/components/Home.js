90e0716b902fbb030f50e5483468f4aaca67b208
BetaNu <ben.dubourguier@gmail.com>
2021-09-24 16:11:03 +0200

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+
+function Home() {
+    return (
+        <Container>
+            <Section
+                title="Model S"
+                description="Order online for touchless delivery"
+                backgroundImg="model-s.jpg"
+                leftBtnText="Custom order"
+                rigthBtnText="Existing inventory"
+            />
+            <Section 
+                title="Model Y"
+                description="Order online for touchless delivery"
+                backgroundImg="model-y.jpg"
+                leftBtnText="Custom order"
+                rigthBtnText="Existing inventory"
+            />
+            <Section 
+                title="Model 3"
+                description="Order online for touchless delivery"
+                backgroundImg="model-3.jpg"
+                leftBtnText="Custom order"
+                rigthBtnText="Existing inventory"
+            />
+            <Section 
+                title="Model X"
+                description="Order online for touchless delivery"
+                backgroundImg="model-x.jpg"
+                leftBtnText="Custom order"
+                rigthBtnText="Existing inventory"
+            />
+            <Section 
+                title="Lowest cost solar panels in America"
+                description = "Money-back guarantee"
+                backgroundImg = "solar-panel.jpg"
+                leftBtnText = "Order now"
+                rigthBtnText = "Learn more"
+            />
+            <Section 
+                title="Solar for now roofs"
+                description = "Solar roofs costs less than a new roof plus solar panels"
+                backgroundImg = "solar-roof.jpg"
+                leftBtnText = "Order now"
+                rigthBtnText = "Learn more"
+            />
+            <Section 
+                title="Accessories"
+                description = ""
+                backgroundImg = "accessories.jpg"
+                leftBtnText = "Shop now"
+            />
+        </Container>
+    )
+}
+
+export default Home
+
+const Container = styled.div`
+    height: 100vh;
+    z-index: 10;
+`
